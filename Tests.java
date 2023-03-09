8481365b9c14895761783149bab6f46191d90a10
Gabriel Bravo <116980060+Lomb2@users.noreply.github.com>
2023-03-09 23:11:03 +0100

    Update Tests.java

--- Tests.java
+++ Tests.java
@@ -1,4 +1,3 @@
-package org.example;
 
 import VestelionKit.VPromise;
 import VestelionKit.VConsole;
@@ -7,59 +6,35 @@ import VestelionKit.VCrypt;
 import java.security.Key;
 
 public class Tests {
-    public static void main(String[] args) throws Exception {
-        //-------------------------------------------------------------------------------------------------------------------
-        //Tests for VCrypt
-        //-------------------------------------------------------------------------------------------------------------------
-
-        Key key = VCrypt.generateKey();
-        String originalText = "Hello, I am a sample text";
-
-        // Encrypt the text
-        String encryptedText = VCrypt.encrypt(originalText, key);
-        VConsole.WriteLine("Original text: %s", originalText);
-        VConsole.WriteLine("Encrypted text: %s", encryptedText);
-
-        // Decrypt the text
-        String decryptedText = VCrypt.decrypt(encryptedText, key);
-        VConsole.WriteLine("Decrypted text: %s", decryptedText);
-
-        // Check if the original text and decrypted text are equal
-        VConsole.WriteLine("Are the original text and decrypted text equal? %s", originalText.equals(decryptedText));
-
-        //-------------------------------------------------------------------------------------------------------------------
-        //Tests for VPromise
-        //-------------------------------------------------------------------------------------------------------------------
-
-        VPromise.Run(
-            //The first lambda is the code we want to run async (try)
-            () -> {
-            VConsole.WriteLine("Executing code asynchronously...");
-
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-            VConsole.WriteLine("Asynchronous code finished.");
-        },
-        //The second lambda is the code we want to be executed in case of Exception in the first lambda (catch)
-        ex -> {
-            // Handle exception
-            System.err.println("An error occurred: " + ex.getMessage());
-        },
-        //The third lambda is the code we want to run ALWAYS after the previous lambdas (finally)
-        () -> {
-            VConsole.WriteLine("The promise has completed.");
+    public static void main(String[] args) throws Exception
+    {
+        // Test VPromise class
+        VPromise.Run(() -> {
+            // This task will succeed
+            VConsole.WriteLine("Task running successfully");
+        }, ex -> {
+            // This exception handler won't be called
+            VConsole.WriteLine("Exception handler called with exception: %s", ex.getMessage());
+        }, () -> {
+            // This completion task will be called
+            VConsole.WriteLine("Completion task called");
         });
 
-        try {
-            Thread.sleep(6000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        VPromise.Run(() -> {
+            // This task will throw an exception
+            throw new RuntimeException("Task failed");
+        }, ex -> {
+            // This exception handler will be called with the thrown exception
+            VConsole.WriteLine("Exception handler called with exception: %s", ex.getMessage());
+        });
 
-        VConsole.Write("End of program");
+        // Test VCrypt class
+        Key key = VCrypt.generateKey();
+        String originalText = "Hello, world!";
+        String encryptedText = VCrypt.encrypt(originalText, key);
+        String decryptedText = VCrypt.decrypt(encryptedText, key);
+        VConsole.WriteLine("Original text: %s", originalText);
+        VConsole.WriteLine("Encrypted text: %s", encryptedText);
+        VConsole.WriteLine("Decrypted text: %s", decryptedText);
     }
 }
