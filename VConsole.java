5cf4c13d387cfcf2048bd3a6d660955b4ed02ad4
Gabriel Bravo <116980060+Lomb2@users.noreply.github.com>
2023-03-10 19:17:04 +0100

    Update VConsole.java

--- VConsole.java
+++ VConsole.java
@@ -18,4 +18,21 @@ public class VConsole
         System.out.println(formatter.format(format, args));
         formatter.close();
     }
+
+    public static void WriteLog(String format, Object... args)
+    {
+        Formatter formatter = new Formatter(Locale.getDefault());
+        String message = formatter.format("(?) " + format, args).toString();
+        formatter.close();
+        System.out.println("\033[32m" + message + "\033[0m");
+    }
+
+    public static void WriteWarning(String format, Object... args)
+    {
+        Formatter formatter = new Formatter(Locale.getDefault());
+        String message = formatter.format("(!) " + format, args).toString();
+        formatter.close();
+        System.out.println("\033[33m" + message + "\033[0m");
+    }
+
 }
