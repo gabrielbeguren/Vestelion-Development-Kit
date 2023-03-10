968754701092d1bbc5c8508132df1207c2b1a9ca
Gabriel Bravo <116980060+Lomb2@users.noreply.github.com>
2023-03-10 18:36:19 +0100

    Update VPromise.java

--- VPromise.java
+++ VPromise.java
@@ -5,9 +5,62 @@ import java.util.function.Consumer;
 
 public class VPromise
 {
-    public static <T> void Run(Runnable task, Consumer<Throwable> exceptionHandler)
+    private Runnable task;
+    private Consumer<Throwable> exceptionHandler;
+    private Runnable completionTask;
+
+    public VPromise(Runnable task, Consumer<Throwable> exceptionHandler) {
+        if (task == null || exceptionHandler == null) {
+            throw new NullPointerException("Argument cannot be null");
+        }
+
+        this.task = task;
+        this.exceptionHandler = exceptionHandler;
+        this.completionTask = null;
+    }
+
+    public VPromise(Runnable task, Consumer<Throwable> exceptionHandler, Runnable completionTask) {
+        if (task == null || exceptionHandler == null || completionTask == null) {
+            throw new NullPointerException("Argument cannot be null");
+        }
+
+        this.task = task;
+        this.exceptionHandler = exceptionHandler;
+        this.completionTask = completionTask;
+    }
+
+    public void RunAsync() throws NullPointerException {
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+            try {
+                task.run();
+            } catch (Throwable ex) {
+                exceptionHandler.accept(ex);
+            }
+        });
+
+        future.whenComplete((result, ex) -> {
+            if (ex != null) {
+                exceptionHandler.accept(ex);
+            } else if (completionTask != null) {
+                completionTask.run();
+            }
+        });
+
+        try {
+            future.join();
+        } catch (Throwable ex) {
+            exceptionHandler.accept(ex);
+        }
+    }
+
+    public static void RunAsync(Runnable task, Consumer<Throwable> exceptionHandler) throws NullPointerException
     {
-        CompletableFuture.runAsync(() ->
+        if (task == null || exceptionHandler == null)
+        {
+            throw new NullPointerException("Argument cannot be null");
+        }
+
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() ->
         {
             try
             {
@@ -18,30 +71,49 @@ public class VPromise
                 exceptionHandler.accept(ex);
             }
         });
+
+        try {
+            future.join();
+        } catch (Throwable ex) {
+            exceptionHandler.accept(ex);
+        }
     }
 
-    public static <T> void Run(Runnable task, Consumer<Throwable> exceptionHandler, Runnable completionTask)
+    public static void RunAsync(Runnable task, Consumer<Throwable> exceptionHandler, Runnable completionTask) throws NullPointerException
     {
-        CompletableFuture.runAsync(() -> 
+        if (task == null || exceptionHandler == null || completionTask == null)
         {
-            try 
+            throw new NullPointerException("Argument cannot be null");
+        }
+
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() ->
+        {
+            try
             {
                 task.run();
-            } 
-            catch (Throwable ex) 
+            }
+            catch (Throwable ex)
             {
                 exceptionHandler.accept(ex);
             }
-        }).whenComplete((result, ex) -> 
+        });
+
+        future.whenComplete((result, ex) ->
         {
-            if (ex != null) 
+            if (ex != null)
             {
                 exceptionHandler.accept(ex);
-            } 
-            else 
+            }
+            else
             {
                 completionTask.run();
             }
         });
+
+        try {
+            future.join();
+        } catch (Throwable ex) {
+            exceptionHandler.accept(ex);
+        }
     }
 }
