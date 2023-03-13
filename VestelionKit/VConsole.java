44fb4d8386f7e78f0a56be0d13edb7c0676b8e2d
Gabriel Bravo <116980060+vestelion@users.noreply.github.com>
2023-03-13 22:07:09 +0100

    Update VConsole.java

--- VestelionKit/VConsole.java
+++ VestelionKit/VConsole.java
@@ -5,17 +5,14 @@ import java.util.Locale;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
-public class VConsole
-{
-    public static void Write(String format, Object... args)
-    {
+public class VConsole {
+    public static void Write(String format, Object... args) {
         Formatter formatter = new Formatter(Locale.getDefault());
         System.out.print(formatter.format(format, args));
         formatter.close();
     }
 
-    public static void WriteLine(String format, Object... args)
-    {
+    public static void WriteLine(String format, Object... args) {
         Formatter formatter = new Formatter(Locale.getDefault());
         System.out.println(formatter.format(format, args));
         formatter.close();
@@ -25,20 +22,16 @@ public class VConsole
     {
         Date now = new Date();
         SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy-HH:mm");
-        Formatter formatter = new Formatter(Locale.getDefault());
-        String message = formatter.format("(?) [%s] %s", dateFormatter.format(now), format, args).toString();
-        formatter.close();
-        System.out.println("\033[32m" + message + "\033[0m");
+        String message = String.format("(?) [%s] %s", dateFormatter.format(now), format);
+        System.out.println("\033[32m" + String.format(message, args) + "\033[0m");
     }
 
     public static void WriteWarning(String format, Object... args)
     {
         Date now = new Date();
         SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy-HH:mm");
-        Formatter formatter = new Formatter(Locale.getDefault());
-        String message = formatter.format("(!) [%s] %s", dateFormatter.format(now), format, args).toString();
-        formatter.close();
-        System.out.println("\033[33m" + message + "\033[0m");
+        String message = String.format("(!) [%s] %s", dateFormatter.format(now), format);
+        System.out.println("\033[33m" + String.format(message, args) + "\033[0m");
     }
 
 }
